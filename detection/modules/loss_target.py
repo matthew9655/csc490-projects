bf98e7a69306aeea0b8dfa2fbb805d9448667b44
yasminehemmati <yasmine.hemmati@mail.utoronto.ca>
2022-03-03 13:18:35 -0500

    vectorized heatmap2

--- detection/modules/loss_target.py
+++ detection/modules/loss_target.py
@@ -72,13 +72,17 @@ def create_heatmap2(grid_coords: Tensor, center: Tensor, h: float, w:float, yaw:
     cov = torch.matmul(scale,rotation) #"Covariance matrix"
     inv  = torch.inverse(cov)
     
-    for i in range(H):
-       for j in range(W):
-           v = grid_coords[i,j,:]
-           diff = (v-c).float()
-           #diff_t = torch.transpose(diff,1, 0)
-           power[i,j]= (-1)*(torch.dot(diff,torch.matmul(inv, diff).float()))
-    
+    #for i in range(H):
+    #  for j in range(W):
+    #       v = grid_coords[i,j,:]
+    #      diff = (v-c).float()
+    #      power[i,j]= (-1)*(torch.dot(diff,torch.matmul(inv, diff).float()))
+    v = grid_coords.reshape((H*W,2))
+    diff = (v-c).float()
+    diff_T= torch.transpose(diff,0,1) # (2 X H*W)
+    A = torch.matmul(inv, diff) # 2X2 multiplied by 2 X H*W 
+    power = (-1)*torch.sum(diff_T*A, dim=0) #1XH*W 
+    power = torch.reshape((H,W))
     heatmap = torch.exp(power)
     vals = torch.flatten(heatmap)
     return heatmap / torch.max(vals)
